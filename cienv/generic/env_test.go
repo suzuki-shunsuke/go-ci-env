dd9727d11bc4249b4254d233758d04e40d4f74bf
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2021-03-05 16:43:56 +0900

    feat: add generic package

---
+++ cienv/generic/env_test.go
@@ -0,0 +1,57 @@
+package generic_test
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"testing"
+	"text/template"
+
+	"github.com/suzuki-shunsuke/go-ci-env/cienv/generic"
+)
+
+func render(k string) (string, error) {
+	tmpl, err := template.New("_").Parse(k)
+	if err != nil {
+		return "", fmt.Errorf("parse template: %w", err)
+	}
+	buf := &bytes.Buffer{}
+	if err := tmpl.Execute(buf, nil); err != nil {
+		return "", nil
+	}
+	if s := strings.TrimSpace(buf.String()); s != "" {
+		return s, nil
+	}
+	return "", nil
+}
+
+func TestClient_CI(t *testing.T) {
+	t.Parallel()
+	data := []struct {
+		caseName string
+		exp      string
+		ci       []string
+		render   func(string) (string, error)
+	}{
+		{
+			caseName: "normal",
+			exp:      "google-cloud-build",
+			ci: []string{
+				"google-cloud-build",
+			},
+			render: render,
+		},
+	}
+	for _, d := range data {
+		d := d
+		t.Run(d.caseName, func(t *testing.T) {
+			t.Parallel()
+			platform := generic.New(generic.Param{
+				CI: d.ci,
+			}, d.render)
+			if a := platform.CI(); a != d.exp {
+				t.Fatalf("wanted %s, got %s", d.exp, a)
+			}
+		})
+	}
+}
